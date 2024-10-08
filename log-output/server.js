2809308c474e9d61bc3f68cd1f5d9f133abd8523
Ariful Islam <ariful6@yahoo.com>
2024-10-09 01:43:17 +0300

    two repo

--- log-output/server.js
+++ log-output/server.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve the HTML file
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "index.html"));
+	res.send("Hello, World!");
 });
 
 // Start the server
--- log-output/src/test.ts
+++
@@ -1,8 +0,0 @@
-function getHashNow() {
-	const randomHash = Math.random().toString(36).substring(2, 10);
-	console.log(randomHash);
-
-	setTimeout(getHashNow, 3000);
-}
-
-getHashNow();
