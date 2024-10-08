20af1f15d13329abac3404ca329f3b763497c449
Ariful Islam <ariful6@yahoo.com>
2024-10-08 21:51:01 +0300

    project v.03

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-import express from "express";
-
-const app = express();
-
+import Koa from "koa";
+const app = new Koa();
 const PORT = process.env.PORT || 3000;
+const createRandomString = () => Math.random().toString(36).substr(2, 6);
 
-app.listen(PORT, () => {
-	console.log("Server started in port", PORT);
+const startingString = createRandomString();
+
+app.use(async (ctx) => {
+	if (ctx.path.includes("favicon.ico")) return;
+
+	const stringNow = createRandomString();
+	console.log("--------------------");
+	console.log(`Responding with ${stringNow}`);
+	ctx.body = `${startingString}: ${stringNow}`;
 });
+
+console.log(`Started with ${startingString}`);
+app.listen(PORT);
